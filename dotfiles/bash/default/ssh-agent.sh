adb4366b9b7747da57ca727b65b9483d23faf121
Erin Sheldon <erin.sheldon@gmail.com>
2011-02-20 10:16:25 -0800

    fix printing in mcurl; don't use keychin on riemann

--- dotfiles/bash/default/ssh-agent.sh
+++ dotfiles/bash/default/ssh-agent.sh
@@ -1,6 +1,9 @@
-res=`which keychain 2>&1> /dev/null`
-if [ $? == 0 ]; then
-	keychain --noinherit id_rsa 2>/dev/null
-	. ~/.keychain/${HOSTNAME}-sh > /dev/null
+#don't do this on riemann
+rmtest=$(uname -n | grep n000)
+if [[ "rmtest" == "" ]]; then
+    res=`which keychain 2>&1> /dev/null`
+    if [ $? == 0 ]; then
+        keychain --noinherit id_rsa 2>/dev/null
+        . ~/.keychain/${HOSTNAME}-sh > /dev/null
+    fi
 fi
-

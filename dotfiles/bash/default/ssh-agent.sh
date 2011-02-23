1aed4473b6554f14b9b43ea7a3442a78479d00a5
Erin Sheldon <erin.sheldon@gmail.com>
2011-02-22 20:23:40 -0500

    fixed bug in ssh-agent checking for riemann

--- dotfiles/bash/default/ssh-agent.sh
+++ dotfiles/bash/default/ssh-agent.sh
@@ -1,6 +1,6 @@
 #don't do this on riemann
 rmtest=$(uname -n | grep n000)
-if [[ "rmtest" == "" ]]; then
+if [[ "$rmtest" == "" ]]; then
     res=`which keychain 2>&1> /dev/null`
     if [ $? == 0 ]; then
         keychain --noinherit id_rsa 2>/dev/null

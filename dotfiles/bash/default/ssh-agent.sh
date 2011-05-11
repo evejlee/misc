965439279e8f8a08bdcc255a762b8bf269709193
Erin Sheldon <erin.sheldon@gmail.com>
2011-05-11 07:11:51 -0700

    docs on mcurl; moved riemann check for keychain to main bashrc

--- dotfiles/bash/default/ssh-agent.sh
+++ dotfiles/bash/default/ssh-agent.sh
@@ -1,9 +1,5 @@
-#don't do this on riemann
-rmtest=$(uname -n | grep n000)
-if [[ "$rmtest" == "" ]]; then
-    res=`which keychain 2>&1> /dev/null`
-    if [ $? == 0 ]; then
-        keychain --noinherit id_rsa 2>/dev/null
-        . ~/.keychain/${HOSTNAME}-sh > /dev/null
-    fi
+res=`which keychain 2>&1> /dev/null`
+if [ $? == 0 ]; then
+    keychain --noinherit id_rsa 2>/dev/null
+    . ~/.keychain/${HOSTNAME}-sh > /dev/null
 fi

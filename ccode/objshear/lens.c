9dcca7a342e5aaaabe125b42ae7ed8859a07bb3c
Erin Sheldon <erin.sheldon@gmail.com>
2011-06-21 17:07:16 -0400

    working on lenses

--- ccode/objshear/lens.c
+++ ccode/objshear/lens.c
@@ -28,6 +28,43 @@ struct lcat* lcat_new(size_t n_lens) {
     return lcat;
 }
 
+struct lcat* lcat_read(const char* filename) {
+    printf("Reading lenses from %s\n", filename);
+    FILE* fptr=fopen(filename,"r");
+    if (fptr==NULL) {
+        printf("Could not open file: %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    int rval;
+    int64 nlens;
+
+    rval=fread(&nlens, sizeof(int64), 1, fptr);
+    printf("Reading %ld lenses\n", nlens);
+    printf("  creating lcat...");
+
+    struct lcat* lcat=lcat_new(nlens);
+
+    struct lens* lens = &lcat->data[0];
+    double ra_rad,dec_rad;
+    for (size_t i=0; i<nlens; i++) {
+        rval=fread(&lens->ra, sizeof(double), 1, fptr);
+        rval=fread(&lens->dec, sizeof(double), 1, fptr);
+        rval=fread(&lens->z, sizeof(double), 1, fptr);
+        // remove these from file since we calculate!
+        rval=fread(&lens->dc, sizeof(double), 1, fptr);
+        rval=fread(&lens->zindex, sizeof(int64), 1, fptr);
+
+        ra_rad = lens->ra*D2R;
+        dec_rad = lens->dec*D2R;
+
+        lens->sinra = sin(ra_rad);
+        lens->cosra = cos(ra_rad);
+        lens->sindec = sin(dec_rad);
+        lens->cosdec = cos(dec_rad);
+
+    }
+}
 
 // use like this:
 //   lcat = lcat_delete(lcat);

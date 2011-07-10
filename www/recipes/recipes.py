175eb4cede27b86eb22867f930668d926d66812f
Erin Sheldon <erin.sheldon@gmail.com>
2011-07-10 00:22:52 -0500

    redirect automatically.

--- www/recipes/recipes.py
+++ www/recipes/recipes.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python2.5
 
 import cgi
-#import os
-#import re
-#import sqlite3 as sqlite
-#import recipe_util as ru
+import os
+import re
+import sqlite3 as sqlite
+import recipe_util as ru
 
 print "Content-type: text/html\n\n"
-print "hello world"
-#ru.print_recipe_book_stats()
+ru.print_recipe_book_stats()

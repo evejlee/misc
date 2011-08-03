fcabdeae02fb2e578f893c59471fe877e3fac49f
Erin Sheldon <erin.sheldon@gmail.com>
2011-08-03 00:54:51 -0400

    formatting

--- ccode/jackknife/jackknife.c
+++ ccode/jackknife/jackknife.c
@@ -20,174 +20,166 @@
 
 ********************************************************************/
 
-int jackknife(float *data,     /* the data to jackknife [Nmeas, Nvar] */
-	      int   Nmeas,     /* Number of measurements of the variables */
-	      int   Nvar,      /* Number of variables */
-	      int   statistic, /* What kind of statistic are we jackknifing?*/
-	      float *datamean, /* the mean values */
-	      float *covariance)  /* covariance [Nvar, Nvar] */
+int jackknife(float *data,      /* the data to jackknife [Nmeas, Nvar] */
+	      int   Nmeas,          /* Number of measurements of the variables */
+	      int   Nvar,           /* Number of variables */
+	      int   statistic,      /* What kind of statistic are we jackknifing?*/
+	      float *datamean,      /* the mean values */
+	      float *covariance)  { /* covariance [Nvar, Nvar] */
 
-{
-  
-  float factor;                 /* factor to get correct variance */
-  int   ivar,jvar,imeas,Ntot;              /* for loops over the variables */
-  double stot,stot2,jtot,smod,smod2;       /* temporary variable for sums */
 
-  double smean, jmean;          /* mean of sample and sub-samples */
-  float tmp;
+    float factor;                 /* factor to get correct variance */
+    int   ivar,jvar,imeas,Ntot;              /* for loops over the variables */
+    double stot,stot2,jtot,smod,smod2;       /* temporary variable for sums */
 
-  float  *sampval;              /* a DAA to to hold the sample values 
-				   [Nmeas, Nvar] */
-  float  *jval;                 /* DAA to hold means over subsamples [Nvar] */
+    double smean, jmean;          /* mean of sample and sub-samples */
+    float tmp;
 
-  /* allocate temporary arrays */
-  Ntot = Nmeas*Nvar;
-  if ( (sampval = calloc(Ntot, sizeof(float)) ) == NULL) 
-    {
-      printf("Error allocating sampval -aborting.");
-      return(-1);
+    float  *sampval;              /* a DAA to to hold the sample values 
+                                     [Nmeas, Nvar] */
+    float  *jval;                 /* DAA to hold means over subsamples [Nvar] */
+
+    /* allocate temporary arrays */
+    Ntot = Nmeas*Nvar;
+    if ( (sampval = calloc(Ntot, sizeof(float)) ) == NULL) {
+        printf("Error allocating sampval -aborting.");
+        return(-1);
     }
-  if ( (jval    = calloc(Nvar, sizeof(float)) ) == NULL)
-    {
-      printf("Error allocating jval -aborting.");
-      free(sampval);
-      return(-1);
+    if ( (jval    = calloc(Nvar, sizeof(float)) ) == NULL) {
+        printf("Error allocating jval -aborting.");
+        free(sampval);
+        return(-1);
     }
 
-  factor = (Nmeas-1.)/Nmeas;
+    factor = (Nmeas-1.)/Nmeas;
 
-  /* loop over the variables */
-  for(ivar=0; ivar<Nvar; ivar++) 
-    {
+    /* loop over the variables */
+    for(ivar=0; ivar<Nvar; ivar++) {
 
-      /* initialize */
-      jtot = 0.0;
+        /* initialize */
+        jtot = 0.0;
 
-      switch(statistic)
-	{
-	case 1:
-	  { /* Mean */
-	    stot = jtotal(data, ivar, Nmeas, 1);
-	    smean = stot/Nmeas;
-	    break;
-	  }
-	case 2:
-	  { /* Standard Deviation */
-	    stot  = jtotal(data, ivar, Nmeas, 1);
-	    stot2 = jtotal(data, ivar, Nmeas, 2);
-	    smean = stot/Nmeas;
-	    smean = ( stot2 - 2.*smean*stot + Nmeas*smean*smean )/(Nmeas-1.);
-	    smean = sqrt(smean);
-	    break;
-	  }
-	case 3:
-	  { /* Variance */
-	    stot  = jtotal(data, ivar, Nmeas, 1);
-	    stot2 = jtotal(data, ivar, Nmeas, 2);
-	    smean = stot/Nmeas;
-	    smean = ( stot2 - 2.*smean*stot + Nmeas*smean*smean )/(Nmeas-1.);
-	    break;
-	  }
-	default: 
-	  { 
-	    printf("No such statistic \"%d\"\n",statistic);
-	    free(jval); free(sampval);
-	    return(-1);
-	  }
-	}
+        switch(statistic) {
+            case 1:
+                { /* Mean */
+                    stot = jtotal(data, ivar, Nmeas, 1);
+                    smean = stot/Nmeas;
+                    break;
+                }
+            case 2:
+                { /* Standard Deviation */
+                    stot  = jtotal(data, ivar, Nmeas, 1);
+                    stot2 = jtotal(data, ivar, Nmeas, 2);
+                    smean = stot/Nmeas;
+                    smean = ( stot2 - 2.*smean*stot + Nmeas*smean*smean )/(Nmeas-1.);
+                    smean = sqrt(smean);
+                    break;
+                }
+            case 3:
+                { /* Variance */
+                    stot  = jtotal(data, ivar, Nmeas, 1);
+                    stot2 = jtotal(data, ivar, Nmeas, 2);
+                    smean = stot/Nmeas;
+                    smean = ( stot2 - 2.*smean*stot + Nmeas*smean*smean )/(Nmeas-1.);
+                    break;
+                }
+            default: 
+                { 
+                    printf("No such statistic \"%d\"\n",statistic);
+                    free(jval); free(sampval);
+                    return(-1);
+                }
+        }
 
-      /* loop over the measurements of this variable 
-	 measure the mean leaving the index imeas out */
-      for(imeas=0; imeas<Nmeas; imeas++) 
-	{
-	  switch(statistic) 
-	    {
-	    case 1:
-	      { /* sample mean */
-		smod = stot - *(data + ivar*Nmeas + imeas);
-		jmean = smod/(Nmeas-1.);
-		*(sampval + ivar*Nmeas + imeas) = jmean;
-		jtot += jmean;
-		break;
-	      }
-	    case 2:
-	      { /* sample standard devation */
-		smod  = stot - *(data + ivar*Nmeas + imeas);
-		smod2 = stot2 - 
-		  ( *(data + ivar*Nmeas + imeas) )*
-		  ( *(data + ivar*Nmeas + imeas) );
-		
-		jmean  = smod/(Nmeas-1.);
-		tmp = 
-		  ( smod2 - 2.*jmean*smod + (Nmeas-1)*jmean*jmean )/(Nmeas-2.);
-		tmp = sqrt(tmp);
-		*(sampval + ivar*Nmeas + imeas) = tmp;
-		jtot += tmp;
-		break;
-	      }
-	    case 3:
-	      { /* sample variance */
-		smod  = stot - *(data + ivar*Nmeas + imeas);
-		smod2 = stot2 - 
-		  ( *(data + ivar*Nmeas + imeas) )*
-		  ( *(data + ivar*Nmeas + imeas) );
+        /* loop over the measurements of this variable 
+           measure the mean leaving the index imeas out */
+        for(imeas=0; imeas<Nmeas; imeas++) {
+            switch(statistic) 
+            {
+                case 1:
+                    { /* sample mean */
+                        smod = stot - *(data + ivar*Nmeas + imeas);
+                        jmean = smod/(Nmeas-1.);
+                        *(sampval + ivar*Nmeas + imeas) = jmean;
+                        jtot += jmean;
+                        break;
+                    }
+                case 2:
+                    { /* sample standard devation */
+                        smod  = stot - *(data + ivar*Nmeas + imeas);
+                        smod2 = stot2 - 
+                            ( *(data + ivar*Nmeas + imeas) )*
+                            ( *(data + ivar*Nmeas + imeas) );
 
-		jmean  = smod/(Nmeas-1.);
+                        jmean  = smod/(Nmeas-1.);
+                        tmp = 
+                            ( smod2 - 2.*jmean*smod + (Nmeas-1)*jmean*jmean )/(Nmeas-2.);
+                        tmp = sqrt(tmp);
+                        *(sampval + ivar*Nmeas + imeas) = tmp;
+                        jtot += tmp;
+                        break;
+                    }
+                case 3:
+                    { /* sample variance */
+                        smod  = stot - *(data + ivar*Nmeas + imeas);
+                        smod2 = stot2 - 
+                            ( *(data + ivar*Nmeas + imeas) )*
+                            ( *(data + ivar*Nmeas + imeas) );
 
-		tmp = 
-		  ( smod2 - 2.*jmean*smod + (Nmeas-1)*jmean*jmean )/(Nmeas-2.);
-		*(sampval + ivar*Nmeas + imeas) = tmp;
-		jtot += tmp;
-		break;
-	      }
-	    default: 
-	      { 
-		printf("No such statistic \"%d\"\n",statistic);
-		free(jval); free(sampval);
-		return(-1);
-	      }
-	    }
-	}
+                        jmean  = smod/(Nmeas-1.);
 
-      /* 
-	 the mean of this value over these samples 
-      */
+                        tmp = 
+                            ( smod2 - 2.*jmean*smod + (Nmeas-1)*jmean*jmean )/(Nmeas-2.);
+                        *(sampval + ivar*Nmeas + imeas) = tmp;
+                        jtot += tmp;
+                        break;
+                    }
+                default: 
+                    { 
+                        printf("No such statistic \"%d\"\n",statistic);
+                        free(jval); free(sampval);
+                        return(-1);
+                    }
+            }
+        }
 
-      *(jval + ivar) = jtot/Nmeas;
+        /* 
+         *  the mean of this value over these samples 
+         */
 
-      /* the jackknife mean */
-      *(datamean + ivar) = smean + (Nmeas-1.)*( smean - *(jval+ivar) );
+        *(jval + ivar) = jtot/Nmeas;
+
+        /* the jackknife mean */
+        *(datamean + ivar) = smean + (Nmeas-1.)*( smean - *(jval+ivar) );
 
     }
 
-  /* 
-     now go through and calculate the covariance matrix 
-     Didn't try to be as efficient as I could; for Nvar << Nmeas
-     That's OK 
-  */
+    /* 
+       now go through and calculate the covariance matrix 
+       Didn't try to be as efficient as I could; for Nvar << Nmeas
+       That's OK 
+    */
 
-  for(jvar=0; jvar<Nvar; jvar++)
-    {
-      for(ivar=jvar; ivar<Nvar; ivar++)
-	{
+    for(jvar=0; jvar<Nvar; jvar++) {
+        for(ivar=jvar; ivar<Nvar; ivar++) {
 
-	  *(covariance + jvar*Nvar + ivar) = 
-	    factor*jtotalcov(sampval, jval, ivar, jvar, Nmeas);
+            *(covariance + jvar*Nvar + ivar) = 
+                factor*jtotalcov(sampval, jval, ivar, jvar, Nmeas);
 	  
-	  if(ivar != jvar) 
-	    {
-	      *(covariance + ivar*Nvar + jvar) = 
-		*(covariance + jvar*Nvar + ivar);
-	    }
+            if(ivar != jvar) {
+                *(covariance + ivar*Nvar + jvar) = 
+                    *(covariance + jvar*Nvar + ivar);
+            }
 	  
-	}
+        }
     }
 
 
-  /* free memory */
-  free(jval); free(sampval);
+    /* free memory */
+    free(jval); 
+    free(sampval);
 
-  return(0);
+    return(0);
 
 }
 

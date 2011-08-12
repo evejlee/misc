68be0d45de5d2503ba34ef83e86f3e7658d13271
Erin Sheldon <erin.sheldon@gmail.com>
2011-08-12 10:50:01 -0400

    tweaking modules stuff

--- dotfiles/bash/astro.bnl.gov/modules.sh
+++ dotfiles/bash/astro.bnl.gov/modules.sh
@@ -1,52 +1,61 @@
 # will want a different one for tutti
-f=~esheldon/local/Modules/3.2.8/init/bash
+f=~astrodat/setup/setup-modules.sh
 if [[ -e $f ]]; then
-    # modules get installed under MODULE_INSTALLS/flavor
-    # will need a different one for tutti, module-installs-tutti
-    export MODULE_INSTALLS=~/local/module-installs
+
     source "$f"
 
+    # those marked with * have platform dependent code, e.g. the are
+    # stand-alone C or extensions for python, etc.
+
+    # this are under $MODULESHOME/modulefiles
+    # and installed under $MODULE_INSTALLS
     module load use.own
 
     module load parallel
+
+    module load pyyaml
+    # also loads pcre      # *
+    module load swig       # *
+
+    module load ccfits     # *
+
+
+    # these are under my ~/privatemodules
+    # and installed generally under ~/exports
+
     module load perllib
     module load shell_scripts
     module load espy
 
     module load desfiles
 
-    module load local
+    module load local      # *
 
-    module load pyyaml
-    
-    # also loads pcre
-    module load swig
+    # this is currently just the python extension
+    module load stomp      # *
 
+    module load esutil     # *
+    module load recfile    # *
 
-    # these are local
-    module load esutil
-    module load recfile
+    module load cosmology  # *
+    module load admom      # *
+    module load fimage     # *
 
-    module load cosmology
-    module load admom
-    module load fimage
+    module load numpydb    # *
 
-    module load numpydb
+    module load scikits_learn # *
 
+    # python only
     module load sdsspy
     module load columns
 
-    # this is currently python only
-    module load stomp
 
-    module load scikits_learn
 
-    module load ccfits
-    module load tmv
+    module load tmv        # *
 
     # prereq: ccfits, tmv, desfiles, esutil
     # also numpy if not using system
-    module load wl/local
+    module load wl/local   # *
 
 fi
 

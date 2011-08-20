2a4a7301a17d3a38ecb4663db4e0ca75cce3cb77
Erin Sheldon <erin.sheldon@gmail.com>
2011-08-20 17:58:12 -0400

    updated addpath and prepend path so that if the variable doesn't exist, just create it

--- dotfiles/bash/default/path.sh
+++ dotfiles/bash/default/path.sh
@@ -1,17 +1,27 @@
 # add to end of path
 append_path()
 {
-  if ! eval test -z "\"\${$1##*:$2:*}\"" -o -z "\"\${$1%%*:$2}\"" -o -z "\"\${$1##$2:*}\"" -o -z "\"\${$1##$2}\"" ; then
-    eval "$1=\$$1:$2"
-  fi
+    # if zero length, just set equal to the input
+    if eval test -z "\$$1"; then 
+        eval "$1=$2"
+    else
+        if ! eval test -z "\"\${$1##*:$2:*}\"" -o -z "\"\${$1%%*:$2}\"" -o -z "\"\${$1##$2:*}\"" -o -z "\"\${$1##$2}\"" ; then
+            eval "$1=\$$1:$2"
+        fi
+    fi
 }
 
 # add to front of path
 prepend_path()
 {
-  if ! eval test -z "\"\${$1##*:$2:*}\"" -o -z "\"\${$1%%*:$2}\"" -o -z "\"\${$1##$2:*}\"" -o -z "\"\${$1##$2}\"" ; then
-    eval "$1=$2:\$$1"
-  fi
+    # if zero length, just set equal to the input
+    if eval test -z "\$$1"; then 
+        eval "$1=$2"
+    else
+        if ! eval test -z "\"\${$1##*:$2:*}\"" -o -z "\"\${$1%%*:$2}\"" -o -z "\"\${$1##$2:*}\"" -o -z "\"\${$1##$2}\"" ; then
+            eval "$1=$2:\$$1"
+        fi
+    fi
 }
 
 export -f append_path

9226687c041e79d4540c4eb70e6b5c435840e86e
Erin Sheldon <erin.sheldon@gmail.com>
2011-08-20 17:43:29 -0400

    add rohan setups

---
+++ dotfiles/bash/rohan/path.sh
@@ -0,0 +1,9 @@
+# we assume these are now defined
+prepend_path PATH ~/shell_scripts
+prepend_path PATH ~/perllib
+prepend_path PATH ~/python/bin
+
+prepend_path PYTHONPATH ~/python
+
+export PATH
+export PYTHONPATH

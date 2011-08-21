cf006a22b66cc9a384c0c07fc2bf51a428be1e94
Erin Sheldon <erin.sheldon@gmail.com>
2011-08-21 15:27:53 -0400

    start agent script

--- dotfiles/bash/rohan/path.sh
+++ dotfiles/bash/rohan/path.sh
@@ -2,8 +2,10 @@
 prepend_path PATH ~/shell_scripts
 prepend_path PATH ~/perllib
 prepend_path PATH ~/python/bin
-
 prepend_path PYTHONPATH ~/python
+
+append_path PATH /sbin
+append_path PATH /usr/sbin
 
 export PATH
 export PYTHONPATH

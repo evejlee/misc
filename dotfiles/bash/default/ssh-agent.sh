66067eef59bc7f1d9e6f452bf407749a02e750b4
Erin Sheldon <erin.sheldon@gmail.com>
2011-08-21 15:28:39 -0400

    going to start the agent by hand

--- dotfiles/bash/default/ssh-agent.sh
+++ dotfiles/bash/default/ssh-agent.sh
@@ -1,8 +1,8 @@
 # need to install keychain and ssh-askpass
 
 # this was moved to shell scripts to be run by hand
-res=`which keychain 2>&1> /dev/null`
-if [ $? == 0 ]; then
-    keychain --noinherit id_rsa 2>/dev/null
-    . ~/.keychain/${HOSTNAME}-sh > /dev/null
-fi
+#res=`which keychain 2>&1> /dev/null`
+#if [ $? == 0 ]; then
+#    keychain --noinherit id_rsa 2>/dev/null
+#    . ~/.keychain/${HOSTNAME}-sh > /dev/null
+#fi

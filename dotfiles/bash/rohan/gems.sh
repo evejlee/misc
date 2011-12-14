a002dd5951b761f4816ed1f6038fe1ab31e5de6d
Erin Sheldon <erin.sheldon@gmail.com>
2011-12-14 13:49:55 -0500

    updated startup files, sup, added gems path stuff

--- dotfiles/bash/rohan/gems.sh
+++ dotfiles/bash/rohan/gems.sh
@@ -1,7 +1,6 @@
 if [[ -e ~/.gem/ruby/1.8/bin ]]; then
     prepend_path PATH ~/.gem/ruby/1.8/bin
 fi
-/var/lib/gems/1.8/bin
 if [[ -e  /var/lib/gems/1.8/bin ]]; then
     prepend_path PATH /var/lib/gems/1.8/bin
 fi

6a3ee1d1195335921c95f4503683603562f5c05d
Erin Sheldon <erin.sheldon@gmail.com>
2012-04-03 19:04:40 -0400

    added all functionality except sort

--- ccode/vector/test.c
+++ ccode/vector/test.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 #include "vector.h"
 
 #define wlog(...) fprintf(stderr, __VA_ARGS__)
@@ -7,23 +8,142 @@
 struct test {
     int id;
     double x;
-}
+};
 
-int main(int argc, char** argv) {
-    wlog("allocating vector\n");
-    struct vector* v = vector_new(25, sizeof(struct test));
 
-    wlog("allocated %lu elements\n", v->size);
+void test_create_and_access() {
+    size_t n=10;
+    struct vector* v = vector_new(n, sizeof(struct test));
+
+    assert(v->size == n);
+    assert(v->capacity == n);
 
     struct test* iter  = vector_front(v);
-    struct test* last = vector_back(v);
-    do {
+    struct test* end = vector_end(v);
+    size_t i=0;
+    while (iter != end) {
+        iter->id = i;
+        iter->x  = 2*i;
         wlog("    id: %d  x: %g\n", iter->id, iter->x);
         iter++;
-    } while (iter != last);
-    /*
-    for (size_t i=0; i<v->size; i++) {
-        wlog("el: %lu  id: %d  x: %g\n", i, v->d[i].id, v->d[i].x);
+        i++;
     }
-    */
+
+    iter = vector_front(v);
+    i=0;
+    while (iter != end) {
+        assert(iter->id == i);
+        assert(iter->x == 2*i);
+        iter++;
+        i++;
+    }
+
+
+    i=7;
+    struct test* el = vector_get(v, i);
+    assert(el->id == i);
+    assert(el->x == 2*i);
+
+    v = vector_delete(v);
+    assert(v == NULL);
+}
+void test_realloc_resize() {
+    size_t n=10;
+    struct vector* v = vector_new(n, sizeof(struct test));
+
+    assert(v->size == n);
+    assert(v->capacity == n);
+
+    size_t new_n = 12;
+    vector_realloc(v, new_n);
+    assert(v->size == n);
+    assert(v->capacity == new_n);
+
+    new_n = 7;
+    vector_realloc(v, new_n);
+    assert(v->size == new_n);
+    assert(v->capacity == new_n);
+
+    size_t rsn = 6;
+    vector_resize(v,rsn);
+    assert(v->size == rsn);
+    assert(v->capacity == new_n);
+
+    rsn = 12;
+    vector_resize(v,rsn);
+    assert(v->size == rsn);
+    assert(v->capacity == rsn);
+
+    vector_clear(v);
+    assert(v->size == 0);
+    assert(v->capacity == rsn);
+
+    vector_freedata(v);
+    assert(v->size == 0);
+    assert(v->capacity == 0);
+    assert(v->d == NULL);
+
+    v = vector_delete(v);
+    assert(v == NULL);
+}
+void test_pushpop() {
+    struct vector* v = vector_new(0, sizeof(struct test));
+
+    size_t i=0;
+    size_t n=10;
+    struct test t;
+    for (i=0; i<n; i++) {
+        t.id = i;
+        t.x = 2*i;
+        vector_push(v, &t);
+    }
+
+    struct test *tptr=NULL;
+    for (i=0; i<n; i++) {
+        tptr = vector_get(v,i);
+        assert(tptr->id == i);
+        assert(tptr->x == 2*i);
+    }
+
+    i=n-1;
+    while (NULL != (tptr=vector_pop(v))) {
+        assert(tptr->id == i);
+        assert(tptr->x == 2*i);
+        i--;
+    }
+
+    assert(v->size == 0);
+    assert(v->capacity >= n);
+
+    v = vector_delete(v);
+    assert(v == NULL);
+}
+
+void test_extend() {
+    struct vector* v = vector_new(0, sizeof(struct test));
+
+    size_t i=0;
+    size_t n=10;
+    struct test* tptr;
+    for (i=0; i<n; i++) {
+        tptr = vector_extend(v);
+        tptr->id = i;
+        tptr->x = 2*i;
+    }
+
+    for (i=0; i<n; i++) {
+        tptr = vector_get(v,i);
+        assert(tptr->id == i);
+        assert(tptr->x == 2*i);
+    }
+
+    v = vector_delete(v);
+    assert(v == NULL);
+}
+
+
+int main(int argc, char** argv) {
+    test_create_and_access();
+    test_realloc_resize();
+    test_pushpop();
 }
--- ccode/vector/vector.h
+++ ccode/vector/vector.h
@@ -16,27 +16,42 @@ struct vector* vector_new(size_t num, size_t elsize);
 // if size > allocated size, then a reallocation occurs
 // if size <= internal size, then only the ->size field is reset
 // use vector_realloc() to reallocate the data vector and set the ->size
-struct vector* vector_resize(struct vector* self, size_t num);
+void vector_resize(struct vector* self, size_t num);
+
+// Set the size to zero.  The memory for the underlying array is not freed.
+void vector_clear(struct vector* self);
+
+// frees the memory allocated for data and sets the size to zero.
+// Use vector_clear(v) if you want to set the visible size to zero
+// without freeing the underlying array.
+
+void vector_freedata(struct vector* self);
+
 
 // perform reallocation on the underlying data vector. This does
 // not change the size field unless the new size is smaller
 // than the viewed size
-struct vector* vector_realloc(struct vector* self, size_t num);
-
-// completely clears memory in the underlying data vector. Use vector_resize(0)
-// if you don't want to clear the memory
-
-void vector_clear(struct vector* self);
+void vector_realloc(struct vector* self, size_t num);
 
 // clears all memory and sets pointer to NULL
 // usage: v=vector_delete(v);
 
 struct vector* vector_delete(struct vector* self);
 
-// push the input onto the end of the vector if reallocation is needed, size is
-// doubled, unless it is empty in case a single element is allocated
+//
+// push a copy of the data pointed to by the input pointer onto the vector.
+//
+// if reallocation is needed, size is doubled, unless it is empty in case a
+// single element is allocated
 
-void vector_push(struct vector* self, int64_t val);
+void vector_push(struct vector* self, void* val);
+
+// same as push but create an empty, zeroed element at the end rather
+// than pushing a particular value on top.  A pointer to the new
+// element is returned so you can modify the data.
+void* vector_extend(struct vector* self);
+
+size_t _vector_new_push_capacity(struct vector* self);
 
 // pop the last element.  You get back a pointer to the underlying data rather
 // than a value.  The size is decremented but no reallocation is performed.  If
@@ -45,11 +60,19 @@ void vector_push(struct vector* self, int64_t val);
 void* vector_pop(struct vector* self);
 
 // sort according to the input comparison function
-void vector_sort(struct vector* self, int (*compar)(const void *, const void *));
+void vector_sort(struct vector* self, int (*cmp)(const void *, const void *));
 
 
+// bounds checked access
+void* vector_get(struct vector* self, size_t i);
+
+// get first or last element. If vector is empty, return null.
 void* vector_front(struct vector* self);
 void* vector_back(struct vector* self);
+
+// don't dereference this!
+void* vector_end(struct vector* self);
+
 //void* vector_find(struct vector* self, void* something);
 
 

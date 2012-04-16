1bf76f87aabf55619870754d40000bb12d90b3bc
Erin Sheldon <erin.sheldon@gmail.com>
2012-04-16 17:59:13 -0400

    guard defs

--- ccode/pymangle/mangle/defs.h
+++ ccode/pymangle/mangle/defs.h
@@ -1,3 +1,6 @@
+#ifndef _MANGLE_DEFINITIONS_H
+#define _MANGLE_DEFINITIONS_H
+
 #define _MANGLE_SMALL_BUFFSIZE 25
 #define _MANGLE_LARGE_BUFFSIZE 255
 #define D2R  0.017453292519943295
@@ -7,3 +10,5 @@
 typedef int64_t int64;
 
 #define wlog(...) fprintf(stderr, __VA_ARGS__)
+
+#endif

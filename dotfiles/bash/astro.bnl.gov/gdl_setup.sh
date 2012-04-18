19556dea265e35ed565050de184a0468178cc734
Erin Sheldon <erin.sheldon@gmail.com>
2012-04-18 09:59:19 -0400

    getting gdl setup again

--- dotfiles/bash/astro.bnl.gov/gdl_setup.sh
+++ dotfiles/bash/astro.bnl.gov/gdl_setup.sh
@@ -1,14 +1,10 @@
 # assume idl_setup was called first
 
-if [ ${GDL_PATH:+1} ]; then
-	GDL_PATH=$GDL_PATH:$IDL_PATH
-else
-	GDL_PATH=$IDL_PATH
-fi
+GDL_PATH=$IDL_PATH
 
 # explicitly include the lib directory from IDL since the GDL library is fairly
 # incomplete.  we payed for the license for this if nothing else.
 
-GDL_PATH=$GDL_PATH:+$IDL_DIR/lib
+append_path GDL_PATH +$IDL_DIR/lib
 
 export GDL_PATH

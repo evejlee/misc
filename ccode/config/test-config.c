ffd467fd8867e614c905fa54e114953aa4fbad82
Erin Sheldon <erin.sheldon@gmail.com>
2012-08-22 09:48:20 -0400

    basic parsing done

--- ccode/config/test-config.c
+++ ccode/config/test-config.c
@@ -147,7 +147,7 @@ int test_nums()
 */
 
 enum cfg_types {
-    CFG_DOUBLE,
+    CFG_DOUBLE=0,
     CFG_LONG,
     CFG_STRING,
     CFG_DBLARR,
@@ -155,9 +155,17 @@ enum cfg_types {
     CFG_STRARR,
     CFG_UNKNOWN
 };
+char *cfg_names[]= {
+    "double",
+    "long",
+    "string",
+    "double[]",
+    "long[]",
+    "string[]"
+};
 struct cfg_entry {
     char *name;
-    char *type_name;
+    char *type_name; // do not free
     enum cfg_types type;
 
     // for scalars
@@ -168,7 +176,7 @@ struct cfg_entry {
     // for arrays
     size_t size;
     double *dblarr;
-    long *lngarr;
+    long *lonarr;
     char **strarr;
 };
 struct cfg_entries {
@@ -188,6 +196,10 @@ struct cfg_entries {
 
 #define CFG_ENTRY_IS_DBLARR(entry) ( (entry)->type == CFG_DBLARR )
 #define CFG_ENTRY_DBLARR(entry) entry->dblarr
+#define CFG_ENTRY_IS_LONARR(entry) ( (entry)->type == CFG_LONARR )
+#define CFG_ENTRY_LONARR(entry) entry->lonarr
+#define CFG_ENTRY_IS_STRARR(entry) ( (entry)->type == CFG_STRARR )
+#define CFG_ENTRY_STRARR(entry) entry->strarr
 
 #define CFG_ENTRY_ARRSIZE(entry) entry->size
 
@@ -214,7 +226,16 @@ enum cfg_types cfg_string2type(const char *type_str)
 }
 static struct cfg_entry *cfg_entry_new(const char *name, enum cfg_types type)
 {
+    //enum cfg_types type=CFG_UNKNOWN;
     struct cfg_entry *entry=NULL;
+
+    /*
+    type = cfg_string2type(type_name);
+    if (type == CFG_UNKNOWN) {
+        return NULL;
+    }
+    */
+
     entry=calloc(1, sizeof(struct cfg_entry));
     if (NULL == entry) {
         fprintf(stderr,"Failed to create config entry\n");
@@ -222,25 +243,35 @@ static struct cfg_entry *cfg_entry_new(const char *name, enum cfg_types type)
     }
 
     entry->type = type;
+    entry->type_name = cfg_names[type];//strdup(type_name);
     entry->name = strdup(name);
 
     return entry;
 }
-static struct cfg_entry *cfg_entry_del(struct cfg_entry *entry)
+
+static char **string_array_free(char **arr, size_t size)
 {
     size_t i=0;
+    if (arr) {
+        for (i=0; i<size; i++) {
+            free(arr[i]);
+        }
+        free(arr);
+    }
+    return NULL;
+}
+static struct cfg_entry *cfg_entry_del(struct cfg_entry *entry)
+{
     if (entry) {
         // We always zero memory on creation, so this is OK
         free(entry->name);
-        free(entry->type_name);
+        //free(entry->type_name);
         free(entry->str);
         free(entry->dblarr);
-        free(entry->lngarr);
+        free(entry->lonarr);
 
         if (entry->strarr != NULL) {
-            for (i=0; i<entry->size; i++) {
-                free(entry->strarr[i]);
-            }
+            entry->strarr = string_array_free(entry->strarr,entry->size);
         }
     }
     return NULL;
@@ -279,9 +310,9 @@ static double *extract_dblarr(char *str, size_t *size, int *status)
             break;
         }
         *size += 1;
-        out = realloc(out, *size);
+        out = realloc(out, (*size)*sizeof(double));
         if (!out) {
-            fprintf(stderr,"failed to allocate string data\n");
+            fprintf(stderr,"failed to allocate double array data\n");
             exit(1);
         }
         out[(*size)-1] = val;
@@ -309,6 +340,39 @@ static long extract_long(char *str, int *status)
     }
     return val;
 }
+static long *extract_lonarr(char *str, size_t *size, int *status)
+{
+    char *ptr=NULL, *endptr=NULL;
+    long val=0;
+    long *out=NULL;
+    *size=0;
+
+    *status=0;
+
+    ptr=str;
+    endptr=ptr;
+    while (1) {
+        val = strtol(ptr, &endptr, 10);
+        if (endptr == ptr) {
+            break;
+        }
+        *size += 1;
+        out = realloc(out, (*size)*sizeof(long));
+        if (!out) {
+            fprintf(stderr,"failed to allocate long array data\n");
+            exit(1);
+        }
+        out[(*size)-1] = val;
+        ptr = endptr;
+    }
+
+    if ((*size) == 0) {
+        fprintf(stderr,"failed to convert any longs in data section: '%s'\n", str);
+        *status=1;
+    }
+    return out;
+}
+
 /*
  * extract a string from between quoted and copy into str.
  * str should not be allocated before calling this function
@@ -346,8 +410,58 @@ static char *extract_string(char *data, int *status)
 _cfg_extract_string_bail:
     return output;
 }
+static char **extract_strarr(char *data, size_t *size, int *status)
+{
+    char *ptr1=NULL, *ptr2=NULL, *str=NULL;
+    char **out=NULL;
 
+    *status=0;
 
+    str=data;
+    while (1) {
+        ptr1 = strchr(str, '"');
+        if (NULL==ptr1) {
+            break;
+        }
+        // skip past the "
+        ptr1++;
+        if (ptr1[0] == '\n' || ptr1[0] == '\0') {
+            fprintf(stderr,"Failed to find closing string quote in: '%s'\n", data);
+            *status=1;
+            break;
+        }
+        ptr2 = strchr(ptr1, '"');
+        if (NULL==ptr2) {
+            fprintf(stderr,"Failed to find closing string quote in: '%s'\n", data);
+            *status=1;
+            break;
+        }
+
+        *size += 1;
+        out = realloc(out, (*size)*sizeof(char*));
+        if (!out) {
+            fprintf(stderr,"failed to allocate string array pointers\n");
+            exit(1);
+        }
+        out[(*size)-1] = calloc(ptr2-ptr1+1, sizeof(char));
+        if (out[(*size)-1] == NULL) {
+            fprintf(stderr,"failed to allocate string array data\n");
+            exit(1);
+        }
+        strncpy(out[(*size)-1], ptr1, ptr2-ptr1);
+
+        str = ptr2+1;
+    }
+
+    if ((*size) == 0) {
+        fprintf(stderr,"failed to convert any strings in data section: '%s'\n", str);
+        *status=1;
+    } else if (*status) {
+        out=string_array_free(out, *size);
+    }
+
+    return out;
+}
 /*
  * data should be from the entry line, pointing after the equals sign
  */
@@ -370,6 +484,16 @@ static struct cfg_entry *cfg_entry_fromdata(const char *name, enum cfg_types typ
 
         case CFG_DBLARR:
             CFG_ENTRY_DBLARR(entry) = extract_dblarr(data, &CFG_ENTRY_ARRSIZE(entry), &status);
+            break;
+        case CFG_LONARR:
+            CFG_ENTRY_LONARR(entry) = extract_lonarr(data, &CFG_ENTRY_ARRSIZE(entry), &status);
+            break;
+        case CFG_STRARR:
+            CFG_ENTRY_STRARR(entry) = extract_strarr(data, &CFG_ENTRY_ARRSIZE(entry), &status);
+            break;
+        case CFG_UNKNOWN:
+            fprintf(stderr,"cannot read CFG_UNKNOWN type\n");
+            status=1;
             break;
     }
 
@@ -440,6 +564,55 @@ static struct cfg_entries *cfg_entries_del(struct cfg_entries *entries)
     return NULL;
 }
 */
+
+static void cfg_entry_print(struct cfg_entry *entry, FILE *stream)
+{
+    size_t n=0, i=0;
+
+    if (!entry) {
+        return;
+    }
+    fprintf(stream,"%s ", entry->type_name);
+    fprintf(stream,"%s = ", entry->name);
+
+    if (CFG_ENTRY_IS_DOUBLE(entry)) {
+        fprintf(stream,"%.16g", CFG_ENTRY_DOUBLE(entry));
+    } else if (CFG_ENTRY_IS_LONG(entry)) {
+        fprintf(stream,"%ld", CFG_ENTRY_LONG(entry));
+    } else if (CFG_ENTRY_IS_STRING(entry)) {
+        fprintf(stream,"\"%s\"", CFG_ENTRY_STRING(entry));
+    } else if (CFG_ENTRY_IS_DBLARR(entry)) {
+        double *darr;
+        darr=CFG_ENTRY_DBLARR(entry);
+        n=CFG_ENTRY_ARRSIZE(entry);
+        for (i=0; i<n; i++) {
+            fprintf(stream,"%.16g",darr[i]);
+            if (i < (n-1))
+                fprintf(stream," ");
+        }
+    } else if (CFG_ENTRY_IS_LONARR(entry)) {
+        long *larr;
+        larr=CFG_ENTRY_LONARR(entry);
+        n=CFG_ENTRY_ARRSIZE(entry);
+        for (i=0; i<n; i++) {
+            fprintf(stream,"%ld",larr[i]);
+            if (i < (n-1))
+                fprintf(stream," ");
+        }
+    } else if (CFG_ENTRY_IS_STRARR(entry)) {
+        char **sarr;
+        sarr=CFG_ENTRY_STRARR(entry);
+        n=CFG_ENTRY_ARRSIZE(entry);
+        for (i=0; i<n; i++) {
+            fprintf(stream,"\"%s\"",sarr[i]);
+            if (i < (n-1))
+                fprintf(stream," ");
+        }
+    }
+
+    fprintf(stream,"\n");
+
+}
 static int process_line(char* line)
 {
     ssize_t i=0;
@@ -466,13 +639,11 @@ static int process_line(char* line)
     // if we get here, we have found a real word
     i = find_white(str);
     strncpy(type_str, str, i);
-    printf("type string: '%s'\n", type_str);
 
     type = cfg_string2type(type_str);
     if (type == CFG_UNKNOWN) {
         return 0;
     }
-    printf("type num: %d\n", type);
 
     str = &str[i];
 
@@ -517,25 +688,10 @@ static int process_line(char* line)
         return 0;
     }
     /*
-     * below here we have memory allocation
+     * below here we have to de-allocate entry
      */
-    printf("name: '%s'\n", entry->name);
-    if (CFG_ENTRY_IS_DOUBLE(entry)) {
-        printf("double: %.16g\n", CFG_ENTRY_DOUBLE(entry));
-    } else if (CFG_ENTRY_IS_LONG(entry)) {
-        printf("long: %ld\n", CFG_ENTRY_LONG(entry));
-    } else if (CFG_ENTRY_IS_STRING(entry)) {
-        printf("string: '%s'\n", CFG_ENTRY_STRING(entry));
-    } else if (CFG_ENTRY_IS_DBLARR(entry)) {
-        double *darr;
-        size_t n=CFG_ENTRY_ARRSIZE(entry), i=0;
-        printf("found %lu doubles\n", n);
-        darr=CFG_ENTRY_DBLARR(entry);
-        for (i=0; i<n; i++) {
-            printf("  darr[%lu]: %.16g\n",i,darr[i]);
-        }
-    }
 
+    cfg_entry_print(entry,stdout);
     entry=cfg_entry_del(entry);
 
     return 1;
@@ -549,18 +705,17 @@ int main(int argc, char *argv[])
     char str_line[] = " string str = \"stuff\"";
 
     char dblarr_line[] = "double[] darr=3.5 2.77 8.00";
-    char bad_line[] = "double[] darr=3";
+    char lonarr_line[] = "long[] larr= 8 -7 25 134\n";
+    char sarr_line[] = "string[] sarr =\"hello there\" \"world\"   \"goodbye\"";
+    char bad_line[] = "string[] sarr =\"hello there\" world\"   \"goodbye\"";
 
-    //char sarr_line[] = "string[] sarr =\"hello there\" \"world\"   \"goodbye\"";
-    //char iarr_line[] = "int x= 5 8 7 10";
-    //char bad_line[] = "string[] = \"hello there\" \"world\"   \"goodbye\"";
-    //char bad_line[] = " string[] sarr = \"stuff\" \"things\"";
-
-    //process_line(sarr_line);
     process_line(dbl_line);
     process_line(long_line);
     process_line(str_line);
     process_line(dblarr_line);
+    process_line(lonarr_line);
+    process_line(sarr_line);
+
     process_line(bad_line);
     
     return 0;

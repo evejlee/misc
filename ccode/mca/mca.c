1506bc4acc040c60767f1abcb32ff22898ad1987
Erin Sheldon <erin.sheldon@gmail.com>
2012-10-13 23:46:39 -0400

    put in basics,need to test

--- ccode/mca/mca.c
+++ ccode/mca/mca.c
@@ -1,21 +1,116 @@
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 #include "mca.h"
 
+struct mca *mca_new(size_t nwalkers,
+                    size_t steps_per_walker,
+                    size_t npars,
+                    double (*lnprob_func)(double *pars),
+                    void *userdata)
+{
+    struct mca *self=calloc(1,sizeof(struct mca));
+    if (self==NULL) {
+        fprintf(stderr,"Could not allocate struct mca\n");
+        exit(EXIT_FAILURE);
+    }
+
+    self->lnprob_func = lnprob_func;
+    self->userdata=userdata;
+
+    self->chain=mca_chain_new(nwalkers,steps_per_walker,npars);
+
+    return self;
+}
+struct mca *mca_del(struct mca *self)
+{
+    if (self) {
+        self->chain=mca_chain_del(self->chain);
+        free(self);
+    }
+    return NULL;
+}
+
+struct mca_chain *mca_chain_new(size_t nwalkers,
+                                size_t steps_per_walker,
+                                size_t npars)
+{
+    struct mca_chain *self=calloc(1,sizeof(struct mca_chain));
+    if (self==NULL) {
+        fprintf(stderr,"Could not allocate struct mca_chain\n");
+        exit(EXIT_FAILURE);
+    }
+
+    self->pars=calloc(nwalkers*steps_per_walker*npars,sizeof(double));
+    if (self->pars==NULL) {
+        fprintf(stderr,"Could not allocate mca_chain pars\n");
+        exit(EXIT_FAILURE);
+    }
+    self->lnprob=calloc(nwalkers*steps_per_walker,sizeof(double));
+    if (self->pars==NULL) {
+        fprintf(stderr,"Could not allocate mca_chain lnprob\n");
+        exit(EXIT_FAILURE);
+    }
+
+    self->nwalkers=nwalkers;
+    self->steps_per_walker=steps_per_walker;
+    self->npars=npars;
+
+    return self;
+
+}
+struct mca_chain *mca_chain_del(struct mca_chain *self)
+{
+    if (self) {
+        free(self->pars);
+        free(self->lnprob);
+        free(self);
+    }
+    return NULL;
+}
+
+void mca_run(struct mca *mca,
+             double **pars, 
+             int nwalkers,
+             int npars)
+{
+
+}
 void mca_stretch_move(double a,
                       const double *pars, 
                       const double *comp_pars, 
-                      size_t npars,
+                      size_t ndim,
                       double *newpars,
                       double *z)
 {
     *z = mca_rand_gofz(a);
-    for (size_t i=0; i<npars; i++) {
+    for (size_t i=0; i<ndim; i++) {
 
         double val=pars[i];
         double cval=comp_pars[i];
 
         newpars[i] = cval + (*z)*(val-cval); 
     }
+}
+
+int mca_accept(int ndim,
+               double lnprob_old,
+               double lnprob_new,
+               double z)
+{
+    double lnprob_diff = (ndim - 1.)*log(z) + lnprob_new - lnprob_old;
+    double r = drand48();
+
+    if (lnprob_diff > log(r)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+                  
+void mca_copy_pars(double *pars_src, double *pars_dst, size_t npars)
+{
+    memcpy(pars_dst, pars_src, npars*sizeof(double));
 }
 
 long mca_rand_long(long n)
--- ccode/mca/mca.h
+++ ccode/mca/mca.h
@@ -1,5 +1,90 @@
+/*
+   mca - a library implementing affine invariant MCMC as
+   outlined in Goodman & Weare 2010.
+
+   Some implementation inspiration from the Emcee python version.
+*/
 #ifndef _MCA_HEADER_GUARD
 #define _MCA_HEADER_GUARD
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct mca_chain {
+    size_t nwalkers;
+    size_t steps_per_walker;
+    size_t npars;
+
+    // indexed by (iwalker,istep,ipar)
+    //    npars*steps_per_walker*iwalker + npars*istep + ipar
+    // where istep is in [0,steps_per_walker)
+    // or ignoring walkers by (istep,ipar)
+    //    npars*istep + ipar
+    // where istep is in [0,nwalkers*nsteps_per_walker)
+
+    double *pars;
+
+    // index by (iwalker,istep)
+    //   steps_per_walker*iwalker + istep
+    // or ignoring walkers by (istep)
+    double *lnprob;
+};
+
+
+struct mca {
+    // holds the pars and lnprob
+    struct mca_chain *chain;
+
+    // This is the function to calculate the log(prob)
+    double (*lnprob_func)(double *pars);
+
+    // The user can store data here for the log(prob) function
+    // This field will not be set or freed by mca_run()
+    void *userdata;
+};
+
+// here istep is within the walker's sub-chain
+#define MCA_PAR_BYWALKER(mca, iwalker, istep, ipar)                 \
+    (mca)->chain->pars[                                             \
+        (mca)->chain->npars*(mca)->chain->steps_per_walker*iwalker  \
+      + (mca)->chain->npars*istep                                   \
+      + ipar                                                        \
+    ]
+
+// here istep is the overall step, [0,nwalkers*steps_per_walker)
+#define MCA_PAR(mca, iwalker, istep, ipar)    \
+    (mca)->chain->pars[                       \
+        (mca)->chain->npars*istep             \
+      + ipar                                  \
+    ]
+
+#define MCA_LNPROB_BYWALKER(mca, iwalker, istep)        \
+    (mca)->chain->lnprob[                               \
+        (mca)->chain->steps_per_walker*iwalker          \
+      + istep                                           \
+    ]
+
+#define MCA_LNPROB(mca, iwalker, istep)    \
+    (mca)->chain->lnprob[istep]
+
+
+
+/*
+*/
+struct mca *mca_new(size_t nwalkers,
+                    size_t steps_per_walker,
+                    size_t npars,
+                    double (*lnprob_func)(double *pars),
+                    void *userdata);
+struct mca *mca_del(struct mca *self);
+
+struct mca_chain *mca_chain_new(size_t nwalkers,
+                                size_t steps_per_walker,
+                                size_t npars);
+struct mca_chain *mca_chain_del(struct mca_chain *self);
+
+
+
 
 /*
    make a stretch move
@@ -12,17 +97,32 @@
    where z is drawn from the mca_rand_gofz, which is
         1/sqrt(z) in the range [1/a,a]
 
-  On return the value of z is set and the
-  newpars are filled in.
+   Note this is a *vectoral* move; z is a scalar in that vectoral equation.
+
+   On return the value of z is set and the newpars are filled in.
 
 */
 void mca_stretch_move(double a,
                       const double *pars, 
                       const double *comp_pars, 
-                      size_t npars,
+                      size_t ndim,
                       double *newpars,
                       double *z);
 
+/*
+   Determine of a stretch move should be accepted
+
+   Returns 1 if yes 0 if no
+*/
+int mca_accept(int ndim,
+               double lnprob_old,
+               double lnprob_new,
+               double z);
+
+/*
+   copy the parameters
+*/
+void mca_copy_pars(double *pars_src, double *pars_dst, size_t npars);
 
 /* 
    generate random integers in [0,n)

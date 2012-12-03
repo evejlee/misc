f6db5bb00b6486b70e4f6004f85f366c95420bbb
Erin Sheldon <erin.sheldon@gmail.com>
2012-12-03 13:44:47 -0800

    added local modules

--- dotfiles/bash/nersc/modules.sh
+++ dotfiles/bash/nersc/modules.sh
@@ -1,4 +1,3 @@
-
 if [[ $hname == "carver" ]]; then
     . /project/projectdirs/cmb/modules/carver/hpcports.sh
     hpcports gnu
@@ -11,15 +10,69 @@ fi
 
 module load use.own
 
-# use specific versions in case different machines have different defaults
-module load python-hpcp
-module load numpy-hpcp
-module load scipy-hpcp
+#
+# global
+#
 
+module load gcc
+module load mkl                             # needed for numpy/lapack
 module load git
 
-# my privatemodules
-module load esutil-ess/local
+#
+# the special hpcp line
+#
+module load scons-hpcp
+module load blas-hpcp
+module load atlas-hpcp
 
-# this is for the module-install script
-export MODULE_INSTALLS=/global/project/projectdirs/des/wl/modules/${hname}/module-installs
+module load readline-hpcp                   # needed by python
+module load python-hpcp
+module load lapack-hpcp                     # needs mkl above
+module load numpy-hpcp
+module load scipy-hpcp
+module load ipython-hpcp
+#module load pyyaml-hpcp
+
+module load cfitsio-hpcp
+#module load ccfits-hpcp
+
+#
+# des module installs
+#
+
+module load libaio                          # * for oracle
+module unload tmv && module load tmv/0.71   # *
+
+module load desoracle
+module load desdb-ess/local
+
+#module load desfiles
+#module load des # create this
+
+#
+# installs in my home
+#
+
+module load perllib
+module load shell_scripts
+module unload espy && module load espy/local
+
+
+
+module load fitsio-ess/local
+
+module unload esutil && module load esutil-ess/local
+
+#module unload wl-ess && module load wl-ess/local   # *
+
+#module load recfile    # *
+
+#module load cosmology  # *
+#module load admom      # *
+#module load fimage/local     # *
+#module load columns
+
+#module load gmix_image/local # *
+#module load emcee
+#module load acor
+

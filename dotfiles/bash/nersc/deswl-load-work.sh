2261e3a37302f66436098879a7bdf43a2f756976
Erin Sheldon <erin.sheldon@gmail.com>
2012-12-07 07:30:48 -0800

    can now do local or work

--- dotfiles/bash/nersc/deswl-load-work.sh
+++ dotfiles/bash/nersc/deswl-load-work.sh
@@ -2,6 +2,14 @@
 #
 # load work versions of esutil, desdb, and deswl
 
-module unload esutil && module load esutil/work
-module unload desdb && module load desdb/work
-module unload deswl && module load deswl/work
+if [[ $# -lt 1 ]]; then
+    echo "usage: source des-load type"
+    echo "  type should be local or work"
+    return
+fi
+
+type=$1
+
+module unload esutil && module load esutil/${type}
+module unload desdb && module load desdb/${type}
+module unload deswl && module load deswl/${type}

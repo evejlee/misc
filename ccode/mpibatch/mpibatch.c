e221914bad5497a2f7892b499f58e09152a401d8
Erin Sheldon <erin.sheldon@gmail.com>
2012-12-09 19:47:51 -0500

    arbitrary command size

--- ccode/mpibatch/mpibatch.c
+++ ccode/mpibatch/mpibatch.c
@@ -11,23 +11,24 @@
    Run all shell scripts in the current directory
      ls *.sh | mpirun -np 4 mpibatch
 
+
+   Note blank lines or fully commented lines still
+   get sent off to the workers as jobs to execute
+
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 
-#define MAX_NAME_SIZE 255
 #define SEND_DATA_TAG 2001
 #define RETURN_DATA_TAG 2002
-#define DONE_TAG 0
 #define MASTER 0
 
 int run_master(int n_workers)
 {
-    //char command[MAX_NAME_SIZE]={0};
     char *command=NULL;
     size_t size=0;
-    int worker=0, ierr=0, imess=0; // dummy
+    int done=0, worker=0, ierr=0, imess=0; // dummy
     MPI_Status status;
 
     int nrunning=0;
@@ -35,12 +36,8 @@ int run_master(int n_workers)
     int have_command=(nread>0);
     while (have_command || nrunning > 0) {
 
-        if (nread > MAX_NAME_SIZE) {
-            fprintf(stderr,"command too long: %d\n", nread);
-            break;
-        }
-        command[nread-1]='\0';  // nread-1 is the newline
-        if (!have_command || nrunning >= (n_workers)) {
+        command[nread-1]='\0';  // to cut junk left at end of string
+        if (!have_command || nrunning >= n_workers) {
             // either no commands left or the queue is full
             ierr = MPI_Recv(&imess, 1, MPI_INT, MPI_ANY_SOURCE, 
                             MPI_ANY_TAG, MPI_COMM_WORLD, &status);
@@ -48,15 +45,18 @@ int run_master(int n_workers)
             nrunning--;
 
             // if we are out of commands, tell this worker we are done
+            // by sending done==0
             if (!have_command) {
-                ierr = MPI_Send("nothing", 8, MPI_CHAR, worker, DONE_TAG,
-                                MPI_COMM_WORLD);
+                ierr = MPI_Send(&done, 1, MPI_INT, worker,
+                                SEND_DATA_TAG, MPI_COMM_WORLD);
             }
         } else {
             // yet to use all workers: send to next in order
             worker += 1;
         }
         if (have_command) {
+            ierr = MPI_Send(&nread, 1, MPI_INT, worker,
+                            SEND_DATA_TAG, MPI_COMM_WORLD);
             ierr = MPI_Send(command, nread, MPI_CHAR, worker,
                             SEND_DATA_TAG, MPI_COMM_WORLD);
             nrunning++;
@@ -70,22 +70,28 @@ int run_master(int n_workers)
 }
 
 int run_worker() {
-    char command[MAX_NAME_SIZE]={0};
+    char *command=NULL;
     MPI_Status status;
-    int ierr=0, imess=0; // imess is dummy
+    int size=0, cmdsize=0, ierr=0, imess=0; // imess is dummy
     while (1) {
-        ierr = MPI_Recv(command, MAX_NAME_SIZE, MPI_CHAR, MASTER,
-                MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-        if (status.MPI_TAG==DONE_TAG) {
+        ierr = MPI_Recv(&cmdsize, 1, MPI_INT, MASTER,
+                        MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        if (cmdsize==0) {
             break;
         }
+        if (cmdsize > size) {
+            command=realloc(command, cmdsize+1);
+            size=cmdsize+1;
+        }
+        ierr = MPI_Recv(command, size, MPI_CHAR, MASTER,
+                MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
         system(command);
 
-        ierr = MPI_Send(&imess, 1, MPI_INT, MASTER, DONE_TAG,
-                MPI_COMM_WORLD);
+        ierr = MPI_Send(&imess, 1, MPI_INT, MASTER, SEND_DATA_TAG,
+                        MPI_COMM_WORLD);
     }
+    free(command);
     return ierr;
 }
 int main(int argc, char **argv)

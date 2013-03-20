4e1afcf603d0c5ae00cd9364f1c7763591ea205d
Erin Sheldon <erin.sheldon@gmail.com>
2013-03-20 15:06:43 -0700

    updated for new des remote

---
+++ dotfiles/bash/nersc/modules-datatran.sh
@@ -0,0 +1,10 @@
+module load use.own
+module load python/2.7.2
+module load perllib
+module load shell_scripts
+module load parallel
+module load espy/local
+# need this case modules not avail on dtn*
+#module load desdb-ess/local
+module load desdb-dtn
+

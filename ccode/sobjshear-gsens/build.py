7fef89ee4ecdd0cef1bb4130a22e1a1a550cef69
Erin Sheldon <erin.sheldon@gmail.com>
2013-04-17 15:58:06 -0400

    just write out same names for exec

--- ccode/sobjshear-gsens/build.py
+++ ccode/sobjshear-gsens/build.py
@@ -7,10 +7,14 @@ import glob
 
 parser = optparse.OptionParser()
 # make an options list, also send to fabricate
-optlist=[optparse.Option('--prefix','-p',default=sys.exec_prefix,help="where to install"),
-         optparse.Option('--hdfs',action="store_true",default=False,help="Files are in hdfs"),
-         optparse.Option('--noopt',action="store_true",help="turn off compiler optimizations"),
-         optparse.Option('--dbg',action="store_true",help="turn on debugging (assert)")]
+optlist=[optparse.Option('--prefix','-p',
+                         default=sys.exec_prefix,help="where to install"),
+         optparse.Option('--hdfs',action="store_true",
+                         default=False,help="Files are in hdfs"),
+         optparse.Option('--noopt',action="store_true",
+                         help="turn off compiler optimizations"),
+         optparse.Option('--dbg',action="store_true",
+                         help="turn on debugging (assert)")]
 parser.add_options(optlist)
 
 options,args = parser.parse_args()
@@ -29,10 +33,12 @@ if not options.dbg:
 
 
 
-sobjshear_sources = ['sconfig', 'config', 'stack', 'Vector','source','lens','cosmo','healpix',
+sobjshear_sources = ['sconfig', 'config', 'stack', 'Vector','source',
+                     'lens','cosmo','healpix',
                      'shear','lensum','histogram','tree','interp','urls',
                      'sobjshear','sdss-survey']
-redshear_sources = ['healpix','cosmo','tree','stack','lens','lensum','sconfig','config',
+redshear_sources = ['healpix','cosmo','tree','stack','lens','lensum',
+                    'sconfig','config',
                     'urls','Vector',
                     'util',
                     'redshear','sdss-survey']
@@ -45,8 +51,8 @@ if options.hdfs:
     LINKFLAGS += ['-L${HADOOP_HOME}/libhdfs','-lhdfs']
     CFLAGS += ['-DHDFS','-I${HADOOP_HOME}/src/c++/libhdfs']
 
-programs = [{'name':'sobjshear-gsens', 'sources':sobjshear_sources},
-            {'name':'redshear-gsens', 'sources':redshear_sources}]
+programs = [{'name':'sobjshear', 'sources':sobjshear_sources},
+            {'name':'redshear', 'sources':redshear_sources}]
 
 
 install_targets = [(prog['name'],'bin') for prog in programs]

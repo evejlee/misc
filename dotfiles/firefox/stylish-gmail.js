41ce84f8b958e74aba41f6226992c489eba3f9b2
Erin Sheldon <erin.sheldon@gmail.com>
2013-09-06 04:56:56 -0400

    added stylish gmail

---
+++ dotfiles/firefox/stylish-gmail.js
@@ -0,0 +1,25 @@
+@namespace url(http://www.w3.org/1999/xhtml);
+
+@-moz-document domain("mail.google.com") {
+
+/* change the font family and font size of body message */
+
+div[class="msg"] {
+
+  font-family: MonoSpace !important;
+
+    font-size: 10pt !important;
+
+}
+
+/* change the font family and font size of the textarea/div field */
+
+textarea, div[class*="LW-avf"] {
+
+  font-family: MonoSpace !important;
+
+    font-size: 10pt !important;
+
+}
+
+}

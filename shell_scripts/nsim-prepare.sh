b77975f364d548ed6078404f5a02e991890c6eec
Erin Sheldon <erin.sheldon@gmail.com>
2014-06-04 11:06:17 -0400

    more on nsim and environments

--- shell_scripts/nsim-prepare.sh
+++ shell_scripts/nsim-prepare.sh
@@ -1,8 +1,15 @@
-# source this before submitting nsim jobs
-module unload wq &> /dev/null
-module unload espy_packages &> /dev/null
-#module load nsim/env
-module load nsim/env2
+# this has become multi-purpose since we cannot make new conda environments
+# that use numba
+
+h=$MODULESHOME
+if [[ $h == "" ]]; then
+    # starting from scratch
+    source /opt/astro/SL64/Modules/default/etc/profile.modules
+    module load use.own
+else
+    module unload wq &> /dev/null
+    module unload espy_packages &> /dev/null
+fi
 
 module unload espy
 module load espy/work
@@ -10,4 +17,8 @@ module load galsim/local
 
 module unload great3-config
 module load great3-config/work
+
+# needs to go last becaus of the prepends
+module load nsim/env2
+
 

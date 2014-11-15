f51304b320bef5646e0c0f584b3d30b3d7928d42
Erin Sheldon <erin.sheldon@gmail.com>
2014-11-14 23:17:25 -0500

    added example config and headers

---
+++ ccode/stackgen/examples/Box.h
@@ -0,0 +1,9 @@
+#ifndef _BOX_HEADER_GUARD
+#define _BOX_HEADER_GUARD
+
+typedef struct {
+    double x1, x2;
+    double y1, y2;
+} Box;
+
+#endif
---
+++ ccode/stackgen/examples/Point.h
@@ -0,0 +1,9 @@
+#ifndef _POINT_HEADER_GUARD
+#define _POINT_HEADER_GUARD
+
+typedef struct {
+    double x;
+    double y;
+} Point;
+
+#endif
